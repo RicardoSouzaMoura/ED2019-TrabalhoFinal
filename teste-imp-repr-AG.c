ee8531cec7e4dcdc53bf3397ebad99d68b4e6159
Ricardo Moura <ricardo.souza.moura@gmail.com>
2019-05-31 08:01:21 -0300

    impressao de representacao da AG

--- teste-imp-repr-AG.c
+++ teste-imp-repr-AG.c
@@ -16,11 +16,22 @@ int main(void){
     TR *ret = criaRetangulo(50, 2);
 
     TAG *arv_gen = NULL;
-    arv_gen = insere_AG(arv_gen, 1, "QUA", qua, 0);
-    arv_gen = insere_AG(arv_gen, 2, "TRI", tri, 1);
-    arv_gen = insere_AG(arv_gen, 3, "CIR", cir, 1);
-    arv_gen = insere_AG(arv_gen, 4, "TRA", tra, 2);
-    arv_gen = insere_AG(arv_gen, 5, "RET", ret, 2);
+    arv_gen = insere_AG(arv_gen, 10, "QUA", qua, 0);
+    arv_gen = insere_AG(arv_gen, 2, "TRI", tri, 10);
+    arv_gen = insere_AG(arv_gen, 3, "CIR", cir, 10);
+    arv_gen = insere_AG(arv_gen, 5, "TRA", tra, 10);
+    arv_gen = insere_AG(arv_gen, 1, "RET", ret, 2);
+    arv_gen = insere_AG(arv_gen, 8, "QUA", qua, 3);
+    arv_gen = insere_AG(arv_gen, 7, "TRI", tri, 3);
+    arv_gen = insere_AG(arv_gen, 9, "CIR", cir, 3);
+    arv_gen = insere_AG(arv_gen, 12, "TRA", tra, 5);
+    arv_gen = insere_AG(arv_gen, 6, "RET", ret, 5);
+    arv_gen = insere_AG(arv_gen, 11, "CIR", cir, 5);
+    arv_gen = insere_AG(arv_gen, 13, "TRA", tra, 3);
+    arv_gen = insere_AG(arv_gen, 14, "RET", ret, 13);
+    arv_gen = insere_AG(arv_gen, 15, "QUA", qua, 13);
+    arv_gen = insere_AG(arv_gen, 16, "TRI", tri, 13);
+    arv_gen = insere_AG(arv_gen, 17, "CIR", cir, 13);
 
     imprime_AG(arv_gen, imprimeItem);
     printf("\n");
